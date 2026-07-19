082cb377495b724acc28c8a22f04ea46e6d71bb2
Ricardo Melo <ricardomarquesmello@gmail.com>
2026-07-19 17:19:35 -0300

    Update supabase-client.js

--- zeus-app/js/supabase-client.js
+++ zeus-app/js/supabase-client.js
@@ -2,4 +2,4 @@
 const SUPABASE_URL = 'https://ypdtjhhsdjqbubekgbsl.supabase.co';
 const SUPABASE_KEY = 'sb_publishable_A_pyJjucZwHaZXhrSdUpWw_t5cZyDks';
 
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
